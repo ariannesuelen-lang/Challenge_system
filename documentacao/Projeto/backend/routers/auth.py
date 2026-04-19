fca4808df84308f03cd477564f946845932bc55c
Mendes <levimendes728@gmail.com>
2026-04-19 16:26:36 -0300

    CriptografiaSenha

--- documentacao/Projeto/backend/routers/auth.py
+++ documentacao/Projeto/backend/routers/auth.py
@@ -1,16 +1,27 @@
 from fastapi import APIRouter, HTTPException
 from backend.database import supabase
 from backend.models import LoginInput, UsuarioCreate
+from backend.security import hash_senha, verificar_senha
 
-router = APIRouter()
+router = APIRouter(prefix="/auth", tags=["Auth"])
 
 
 @router.post("/login")
 def login(dados: LoginInput):
-    res = supabase.table("usuarios").select("id, nome, email, tipo_usuario").eq("email", dados.email).eq("senha", dados.senha).execute()
+    res = supabase.table("usuarios").select("*").eq("email", dados.email).execute()
     if not res.data:
         raise HTTPException(status_code=401, detail="Credenciais inválidas")
-    return {"mensagem": "Login realizado com sucesso", "usuario": res.data[0]}
+    usuario = res.daat[0]
+
+    if not verificar_senha(dados.senha, ususario["senha"]):
+        raise HTTPException(status_code=401, detail="Credenciais inválidas")
+
+    usuario.pop("senha", None)
+
+    return{
+        "mensagem": "Login realizado com sucesso",
+        "usuario": usuario
+    }
 
 
 @router.post("/usuarios")
@@ -18,10 +29,17 @@ def criar_usuario(dados: UsuarioCreate):
     existe = supabase.table("usuarios").select("id").eq("email", dados.email).execute()
     if existe.data:
         raise HTTPException(status_code=409, detail="Email já cadastrado")
+    senha_hash = hash_senha(dados.senha)
     res = supabase.table("usuarios").insert({
         "nome": dados.nome,
         "email": dados.email,
-        "senha": dados.senha,
+        "senha": senha_hash,
         "tipo_usuario": dados.tipo_usuario
     }).execute()
-    return {"mensagem": "Usuário criado com sucesso", "usuario": res.data[0]}
+    usuario = res.data[0]
+    usuario.pop("senha", None)
+
+    return {
+        "mensagem": "Usuário criado com sucesso", 
+        "usuario": usuario
+    }
