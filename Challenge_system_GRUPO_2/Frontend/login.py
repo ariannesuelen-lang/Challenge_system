03c0448cd232eb46b9d6189f5407a533a9f9d9df
SarahPaoliello <sarahpaoliello@gmail.com>
2026-05-11 19:40:39 -0300

    Update login.py

--- Challenge_system_GRUPO_2/Frontend/login.py
+++ Challenge_system_GRUPO_2/Frontend/login.py
@@ -19,8 +19,9 @@ supabase = get_supabase()
 # ─────────────────────────────────────────
 # SESSION STATE
 # ─────────────────────────────────────────
-if "pagina" not in st.session_state:
-    st.session_state.pagina = "login"
+if "pagina_login" not in st.session_state:
+    st.session_state.pagina_login = "login"
+    
 if "usuario_logado" not in st.session_state:
     st.session_state.usuario_logado = None
 
