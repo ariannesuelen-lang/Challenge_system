cef7378a2e97e9b7d4916fc0d9e8a2467e2887bd
Assiralaris <lpdacassiral@gmail.com>
2026-05-25 11:42:12 -0300

    Update cadastro_perguntas.py

--- telas/mini_provas/cadastro_perguntas.py
+++ telas/mini_provas/cadastro_perguntas.py
@@ -1,18 +1,25 @@
 import streamlit as st
-from services.mini_prova_service import criar_questao
-
 
 def tela_cadastro_perguntas():
+
     st.title("Cadastro de Perguntas")
 
     disciplina = st.text_input("Disciplina")
+
     assunto = st.text_input("Assunto")
+
     dificuldade = st.selectbox(
         "Dificuldade",
-        ["facil", "media", "dificil"]
+        [
+            "facil",
+            "media",
+            "dificil"
+        ]
     )
 
     pergunta = st.text_area("Pergunta")
+
+    st.subheader("Alternativas")
 
     alternativa_a = st.text_input("Alternativa A")
     alternativa_b = st.text_input("Alternativa B")
@@ -21,24 +28,19 @@ def tela_cadastro_perguntas():
     alternativa_e = st.text_input("Alternativa E")
 
     resposta_correta = st.selectbox(
-        "Resposta correta",
+        "Resposta Correta",
         ["a", "b", "c", "d", "e"]
     )
 
     if st.button("Cadastrar Pergunta"):
-        dados = {
-            "disciplina": disciplina,
-            "assunto": assunto,
-            "dificuldade": dificuldade,
-            "pergunta": pergunta,
-            "alternativa_a": alternativa_a,
-            "alternativa_b": alternativa_b,
-            "alternativa_c": alternativa_c,
-            "alternativa_d": alternativa_d,
-            "alternativa_e": alternativa_e,
-            "resposta_correta": resposta_correta
-        }
 
-        criar_questao(dados)
+        st.success(
+            "Pergunta cadastrada visualmente"
+        )
 
-        st.success("Pergunta cadastrada com sucesso")
+    st.divider()
+
+    if st.button("Voltar"):
+
+        st.switch_page(
+            "telas/mini_provas/mini_provas_professor.py"
