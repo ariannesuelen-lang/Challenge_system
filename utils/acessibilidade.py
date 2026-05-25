e1e736ae7735ffa4748ad1cedec950b0ea331046
SarahPaoliello <sarahpaoliello@gmail.com>
2026-05-25 16:49:20 -0300

    Update acessibilidade.py

--- utils/acessibilidade.py
+++ utils/acessibilidade.py
@@ -1,50 +1,26 @@
 import streamlit as st
 
 
-def configurar_acessibilidade():
+def aplicar_alto_contraste():
 
-    if "alto_contraste" not in st.session_state:
-        st.session_state["alto_contraste"] = False
-
-    if "tempo_extra" not in st.session_state:
-        st.session_state["tempo_extra"] = 0
-
-    st.sidebar.title("Acessibilidade")
-
-    alto_contraste = st.sidebar.checkbox(
-        "Alto contraste"
-    )
-
-    tempo_extra = st.sidebar.selectbox(
-        "Tempo extra por questão",
-        [0, 30, 60]
-    )
-
-    leitura = st.sidebar.checkbox(
-        "Leitura da questão"
-    )
-
-    st.session_state[
+    if st.session_state.get(
         "alto_contraste"
-    ] = alto_contraste
-
-    st.session_state[
-        "tempo_extra"
-    ] = tempo_extra
-
-    st.session_state[
-        "leitura"
-    ] = leitura
-
-    if alto_contraste:
+    ):
 
         st.markdown(
             """
             <style>
+
             .stApp {
                 background-color: black;
                 color: white;
             }
+
+            button {
+                background-color: white !important;
+                color: black !important;
+            }
+
             </style>
             """,
             unsafe_allow_html=True
