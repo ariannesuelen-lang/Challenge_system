5735fc127100d243c2d8ba1877c3acf2149171d9
SarahPaoliello <sarahpaoliello@gmail.com>
2026-05-25 17:01:50 -0300

    Update mini_provas_professor.py

--- telas/mini_provas/mini_provas_professor.py
+++ telas/mini_provas/mini_provas_professor.py
@@ -3,40 +3,80 @@ import streamlit as st
 
 def tela_mini_provas_professor():
 
-    st.title("Painel de Mini Provas")
+    st.title(
+        "Painel do Professor"
+    )
 
-    st.subheader("Área do Professor")
-
-    col1, col2 = st.columns(2)
+    col1, col2, col3 = st.columns(3)
 
     with col1:
 
-        if st.button("Cadastrar Perguntas"):
-            st.switch_page(
-                "telas/mini_provas/cadastro_perguntas.py"
+        if st.button(
+            "Cadastrar Perguntas"
+        ):
+
+            st.session_state.pagina = (
+                "cadastro_perguntas"
             )
 
-        if st.button("Cadastrar Mini Prova"):
-            st.switch_page(
-                "telas/mini_provas/cadastro_mini_provas.py"
-            )
+            st.rerun()
 
     with col2:
 
-        if st.button("Solicitações de Reabertura"):
-            st.switch_page(
-                "telas/mini_provas/solicitacoes_reabertura.py"
+        if st.button(
+            "Cadastrar Mini Prova"
+        ):
+
+            st.session_state.pagina = (
+                "cadastro_mini_provas"
             )
 
-        if st.button("Notificações"):
-            st.switch_page(
-                "telas/mini_provas/notificacoes_mini_provas.py"
+            st.rerun()
+
+    with col3:
+
+        if st.button(
+            "Solicitações"
+        ):
+
+            st.session_state.pagina = (
+                "solicitacoes_reabertura"
             )
+
+            st.rerun()
 
     st.divider()
 
-    st.subheader("Mini Provas Criadas")
-
-    st.info(
-        "Aqui futuramente aparecerão as mini provas cadastradas"
+    st.subheader(
+        "Mini Provas Criadas"
     )
+
+    for i in range(3):
+
+        with st.container(border=True):
+
+            st.write(
+                f"Mini prova {i+1}"
+            )
+
+            st.write(
+                "Disciplina: Matemática"
+            )
+
+            st.write(
+                "5 perguntas"
+            )
+
+            col1, col2 = st.columns(2)
+
+            with col1:
+
+                st.button(
+                    f"Editar {i}"
+                )
+
+            with col2:
+
+                st.button(
+                    f"Visualizar {i}"
+                )
