bef685b16625dad8900cc5a3b6e739317024abfc
Assiralaris <lpdacassiral@gmail.com>
2026-05-25 11:44:49 -0300

    Update notificacoes_mini_provas.py

--- telas/mini_provas/notificacoes_mini_provas.py
+++ telas/mini_provas/notificacoes_mini_provas.py
@@ -1,1 +1,24 @@
+import streamlit as st
 
+
+def tela_notificacoes_mini_provas():
+
+    st.title("Notificações")
+
+    notificacoes = [
+        "Sua solicitação foi aprovada",
+        "Nova mini prova disponível",
+        "Resultado liberado"
+    ]
+
+    for notificacao in notificacoes:
+
+        st.info(notificacao)
+
+    st.divider()
+
+    if st.button("Voltar"):
+
+        st.switch_page(
+            "telas/mini_provas/mini_provas_professor.py"
+        )
