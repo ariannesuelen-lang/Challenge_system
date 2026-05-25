18a2f3b2d518f244f23c972f332d3c335660360d
Assiralaris <lpdacassiral@gmail.com>
2026-05-25 11:45:09 -0300

    Update solicitacoes_reabertura.py

--- telas/mini_provas/solicitacoes_reabertura.py
+++ telas/mini_provas/solicitacoes_reabertura.py
@@ -1,1 +1,36 @@
+import streamlit as st
 
+
+def tela_solicitacoes_reabertura():
+
+    st.title("Solicitações de Reabertura")
+
+    for i in range(2):
+
+        with st.container(border=True):
+
+            st.write("Aluno: João")
+
+            st.write(
+                "Motivo: minha internet caiu"
+            )
+
+            col1, col2 = st.columns(2)
+
+            with col1:
+
+                if st.button(f"Aprovar {i}"):
+                    st.success("Solicitação aprovada")
+
+            with col2:
+
+                if st.button(f"Recusar {i}"):
+                    st.error("Solicitação recusada")
+
+    st.divider()
+
+    if st.button("Voltar"):
+
+        st.switch_page(
+            "telas/mini_provas/mini_provas_professor.py"
+        )
